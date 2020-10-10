57d6a347c9394768967de65652ea98901f487083
Andrzej Kajetanowicz <andrzej.kajetanowicz@gmail.com>
2020-10-10 12:45:59 +0200

    Add SpaceInvaders#each_area specs

--- lib/space_invaders/radar_sample.rb
+++ lib/space_invaders/radar_sample.rb
@@ -2,18 +2,25 @@ module SpaceInvaders
   class RadarSample < Bitmap
 
     def each_area(area_width, area_height)
-      raise StadardError "Area is to wide - current width is #{width} vs. #{area_width}" if width < area_width
-      raise StadardError "Area is to wide - current width is #{height} vs. #{area_height}" if height < area_height
+      raise StandardError, "Area is too wide - current width is #{width} vs. #{area_width}" if width < area_width
+      raise StandardError, "Area is too high - current width is #{height} vs. #{area_height}" if height < area_height
 
       (0..(width - area_width)).each do |x|
         (0..(height - area_height)).each do |y|
-          yield Area.new(
-            self,
-            Point.new(x, y),
-            Point.new(x + area_width - 1, y + area_height - 1)
-          )
+          yield build_area(x, y, area_width, area_height)
+
         end
       end
+    end
+
+    private
+
+    def build_area(x, y, area_width, area_height)
+      Area.new(
+        self,
+        Point.new(x, y),
+        Point.new(x + area_width - 1, y + area_height - 1)
+      )
     end
   end
 end
--- spec/space_invaders/radar_sample_spec.rb
+++ spec/space_invaders/radar_sample_spec.rb
@@ -1,7 +1,35 @@
 require 'space_invaders'
 
 describe SpaceInvaders::RadarSample do
+  subject(:radar_sample) do
+    described_class.from_string(<<~ASCII)
+      ----------
+      ----------
+      ----------
+      ----------
+      ----------
+      ----------
+    ASCII
+  end
+
   describe '#each_area' do
-    # TODO
+    it 'yields block with each area of specified size' do
+      expect { |b| radar_sample.each_area(8, 5, &b) }.to yield_successive_args(
+        kind_of(SpaceInvaders::Area), kind_of(SpaceInvaders::Area), kind_of(SpaceInvaders::Area),
+        kind_of(SpaceInvaders::Area), kind_of(SpaceInvaders::Area), kind_of(SpaceInvaders::Area),
+      )
+    end
+
+    context 'when specified area is to wide' do
+      it 'raises an exception' do
+        expect { |b| radar_sample.each_area(11, 1) }.to raise_error(StandardError, /too wide/)
+      end
+    end
+
+    context 'when specified area is to high' do
+      it 'raises an exception' do
+        expect { |b| radar_sample.each_area(4, 10) }.to raise_error(StandardError, /too high/)
+      end
+    end
   end
 end
